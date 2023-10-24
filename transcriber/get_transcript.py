85e55255c9e61f4efca1befbcbd7a7814e81205e
SNG Viraj Reddy <vir200319@gmail.com>
2023-10-24 18:11:49 +0530

    Add .env variable for APIKEY

--- transcriber/get_transcript.py
+++ transcriber/get_transcript.py
@@ -1,8 +1,9 @@
 # importing packages 
 import assemblyai as aai
 import re
+import os
 # Replace with your API token
-aai.settings.api_key = f"9378af6f47394df79bde84c17c4bae50"
+aai.settings.api_key = os.environ['AAI_APIKEY']
 
 # URL of the file to transcribe
 def Transcribe(path):
