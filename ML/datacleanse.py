376457fa19360d3906073a093503448e0f59ab39
bhavika varshney <bhavika varshney>
2023-10-24 16:10:08 +0530

    data cleaning

---
+++ ML/datacleanse.py
@@ -0,0 +1,27 @@
+# Importing libraries
+import numpy as np
+np.__version__
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Dataset import here
+dataset = pd.read_csv('./Dataset/Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the text
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+  review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+  review = review.lower()
+  review = review.split()
+  ps = PorterStemmer()
+  all_stopwords = stopwords.words('english')
+  all_stopwords.remove('not')
+  review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+  review = ' '.join(review)
+  corpus.append(review)
+print(corpus)
