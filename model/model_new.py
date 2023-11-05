42edd338ce3e5bcaa4f0f1bdbbcaab0b52efbbf1
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-11-06 01:33:20 +0530

    code to use transcriptions from JSON file instead of PDF

--- model/model_new.py
+++ model/model_new.py
@@ -1,46 +1,31 @@
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+import sys
+import json
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFaceHub
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFaceHub
+
+with open('./transcriber/transcriptions.json', 'r') as json_file:
+    data = json.load(json_file)
 
 repo_id = "mistralai/Mistral-7B-v0.1"
-llm = HuggingFaceHub(huggingfacehub_api_token='your huggingface access token here', 
-                     repo_id=repo_id, model_kwargs={"temperature":0.2, "max_new_tokens":50})
+llm = HuggingFaceHub(huggingfacehub_api_token='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', repo_id=repo_id, model_kwargs={"temperature": 0.2, "max_new_tokens": 50})
 
-# Load the pdf file and split it into smaller chunks
-loader = PyPDFLoader('report.pdf')
-documents = loader.load()
+for text in data:
+    embeddings = HuggingFaceEmbeddings()
 
-# Split the documents into smaller chunks 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+    db = Chroma.from_texts(text, embeddings)
+    retriever = db.as_retriever(search_kwargs={'k': 2})
 
-# We will use HuggingFace embeddings 
-embeddings = HuggingFaceEmbeddings()
+    qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever, return_source_documents=True)
 
-#Using Chroma vector database to store and retrieve embeddings of our text
-db = Chroma.from_documents(texts, embeddings)
-retriever = db.as_retriever(search_kwargs={'k': 2}) 
-
-# We are using Mistral-7B for this question answering 
-repo_id = "mistralai/Mistral-7B-v0.1"
-llm = HuggingFaceHub(huggingfacehub_api_token='your huggingface access token here', 
-                     repo_id=repo_id, model_kwargs={"temperature":0.2, "max_new_tokens":50})
-
-# Create the Conversational Retrieval Chain
-qa_chain = ConversationalRetrievalChain.from_llm(llm, retriever,return_source_documents=True)
-
-#We will run an infinite loop to ask questions to LLM and retrieve answers untill the user wants to quit
-import sys
-chat_history = []
-while True:
-    query = input('Prompt: ')
-    #To exit: use 'exit', 'quit', 'q', or Ctrl-D.",
-    if query.lower() in ["exit", "quit", "q"]:
-        print('Exiting')
-        sys.exit()
-    result = qa_chain({'question': query, 'chat_history': chat_history})
-    print('Answer: ' + result['answer'] + '\n')
-    chat_history.append((query, result['answer']))
+    chat_history = []
+    while True:
+        query = input('Prompt: ')
+        # To exit: use 'exit', 'quit', 'q', or Ctrl-D."
+        if query.lower() in ["exit", "quit", "q"]:
+            print('Exiting')
+            sys.exit()
+        result = qa_chain({'question': query, 'chat_history': chat_history})
+        print('Answer: ' + result['answer'] + '\n')
+        chat_history.append((query, result['answer']))
