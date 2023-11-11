e770a30e3b4781f46d04ddfdc3143c76bbd04f78
SNG Viraj Reddy <vir200319@gmail.com>
2023-11-11 12:36:50 +0530

    add context box to site, and format
    Signed-off-by: SNG Viraj Reddy <vir200319@gmail.com>

--- src/text_processing/summarizer.py
+++ src/text_processing/summarizer.py
@@ -1,29 +1,38 @@
 import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
-model_name = 'google/pegasus-xsum'
-torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model_name = "google/pegasus-xsum"
+torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 
 # Suppress initialization warning
 with torch.no_grad():
     model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
 
-def generate_math_summary(text):
-    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding='longest')
-    translated = model.generate(**inputs)
+
+def generate_math_summary(
+    text, max_length=500, num_beams=5, length_penalty=2.0, no_repeat_ngram_size=2
+):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding="longest")
+    translated = model.generate(
+        **inputs,
+        max_length=max_length,
+        num_beams=num_beams,
+        length_penalty=length_penalty,
+        no_repeat_ngram_size=no_repeat_ngram_size
+    )
     tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
     return tgt_text[0]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from pathlib import Path
 
     base_path = Path.cwd()
-    data_path = base_path / 'data' / 'summarize.txt'
+    data_path = base_path / "data" / "summarize.txt"
 
-    with open(data_path, 'r') as f:
+    with open(data_path, "r") as f:
         text = f.read()
-    
+
     summary = generate_math_summary(text)
     print(summary)
-
--- src/webapp/app-viraj.py
+++ src/webapp/app-viraj.py
@@ -4,6 +4,7 @@ import time
 
 from src.model.modelv2 import initialize_qa_chain, ask_question
 from src.text_processing.transcriber import transcribe_youtube_video
+from src.text_processing.summarizer import generate_math_summary
 
 
 def gradio_chatbot():
@@ -12,7 +13,8 @@ def gradio_chatbot():
         head="Saido is a chatbot that can answer questions about a YouTube video.",
         theme=gr.themes.Monochrome(),
     ) as demo:
-        gr.Markdown("""
+        gr.Markdown(
+            """
         <div style="text-align: center; font-family: Arial, sans-serif; color: #000;">
             <h1 style="border-bottom: 2px solid #754940; display: inline-block; padding-bottom: 0px;">Saido</h1>
             <p style="font-size: 18px; margin-top: 5px; color: #666;">Conversations, Transformed through Video</p>
@@ -20,27 +22,26 @@ def gradio_chatbot():
         """
         )
 
-        url = gr.Textbox(label="YouTube URL", lines=1, placeholder="Enter YouTube URL here")
+        url = gr.Textbox(
+            label="YouTube URL", lines=1, placeholder="Enter YouTube URL here"
+        )
         transcription_text = gr.Textbox(label="transcription", visible=False)
-        
+        summarized_text = gr.Textbox(label="Video context")
+
         # Chatbot block
         chatbot = gr.Chatbot()
         msg = gr.Textbox(placeholder="Type your message here...")
         clear = gr.ClearButton([msg, chatbot])
 
-        def respondt(url, transcription_text):
+        def respondt(url, transcription_text, summarized_text):
             transcribe_text = transcribe_youtube_video(url)
+            summary = generate_math_summary(transcribe_text)
             time.sleep(2)
-            return url, transcribe_text
+            return url, transcribe_text, summary
 
         def respondc(message, chat_history):
             qa_chain = initialize_qa_chain(
-                [
-                    {
-                        'url': url.value,
-                        'transcript': transcription_text.value
-                    }
-                ]
+                [{"url": url.value, "transcript": transcription_text.value}]
             )
             bot_message = ask_question(qa_chain, message)
             chat_history.append((message, bot_message))
@@ -48,12 +49,19 @@ def gradio_chatbot():
 
             return "", chat_history
 
-        url.submit(respondt, [url, transcription_text], [url, transcription_text])
+        url.submit(
+            respondt,
+            [url, transcription_text, summarized_text],
+            [url, transcription_text, summarized_text],
+        )
         msg.submit(respondc, [msg, chatbot], [msg, chatbot])
 
-        gr.Markdown('**<p style="font-size: 18px; margin-top: 10px; color: #666;"><center>Made with ❤️ by AI/ML 45</center></p>**')
+        gr.Markdown(
+            '**<p style="font-size: 18px; margin-top: 10px; color: #666;"><center>Made with ❤️ by AI/ML 45</center></p>**'
+        )
 
         demo.launch()
+
 
 if __name__ == "__main__":
     gradio_chatbot()
